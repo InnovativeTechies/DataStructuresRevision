47209f5a44f7551f40293475edbf9f708f13f5aa
Purple Sense <dev@purplesense.com>
2020-04-15 00:14:33 +0530

    Add project files.

---
+++ LinkedList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleLinkedList sl = new SimpleLinkedList();
+
+            sl.AddToLinkedListLast(1);
+            sl.AddToLinkedListLast(2);
+            sl.AddToLinkedListLast(3);
+            sl.AddToLinkedListLast(4);
+
+            sl.ReadAll();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ LinkedList/SimpleLinkedList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+
+    public class Node
+    {
+        public object data { get; set; }
+        public Node nextNode = null;
+    }
+    public class SimpleLinkedList
+    {
+        Node Head = null;
+        Node Tail = null;
+        public void AddToLinkedListLast(object data)
+        {
+            Node newItem = new Node();
+            newItem.data = data;
+            if (Head==null)
+            {
+                
+                
+                Head = newItem;
+                Tail = newItem;
+                
+
+            }
+            else
+            {
+
+                Tail.nextNode = newItem;
+                Tail = newItem;
+               
+            }
+        }
+
+        public void AddToLinkedListFirst(object data)
+        {
+            Node newItem = new Node();
+            newItem.data = data;
+            newItem.nextNode = Head;
+            Head = newItem;
+        }
+
+        public void ReadAll()
+        {
+            Node current = Head;
+            while (current.nextNode!=null)
+            {
+                Console.WriteLine(current.data);
+                current = current.nextNode;
+                
+               
+            }
+            Console.WriteLine(current.data);
+        }
+
+    }
+}
